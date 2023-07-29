1ba974c03710a15d216c34b2948a692245ab565d
sahishpandav <99825391+sahishpandav@users.noreply.github.com>
2023-07-29 12:21:31 +0530

    Update BubbleSort.java
    if arr[i] > arr[i+1] swap them. To place the element in their respective position, we have to do the following operation N-1 times. 
    Time Complexity: O(N2)

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,3 +1,23 @@
-import java.util.*
+import java.util.*;
 
-  
+public class bubbleSort {
+        public static void main(String args[]){
+        int[] arr = {7, 8, 5, 2, 1};
+        // loop runs n - 1 times in bubble sort
+        for(int i = 0; i < arr.length - 1; ++i){
+            for(int j = 0; j < arr.length - i - 1; ++j){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for(int nums: arr){
+            System.out.print(nums + " ");
+        }
+
+    }
+
+}
